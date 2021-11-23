5a5c09bd1b95a54631ac5881c9a6941af0d334c9
RebecaDumitrascuta <rebecadumitrascuta02@gmail.com>
2021-11-23 16:34:15 +0200

    Add input validations, with msg alert (todo: change to inline message)

--- offices/urls.py
+++ offices/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from offices.views import get_available_offices, book_now, do_booking, back_button, my_bookings,cancel_booking
+from offices.views import get_available_offices, book_now, save_booking, back_button, my_bookings,cancel_booking
 
 
 app_name = 'offices'
@@ -7,9 +7,9 @@ app_name = 'offices'
 urlpatterns = [
     path('', get_available_offices, name='ReservationFormView'),
     path('reserveNow/<int:id>/<str:start_date>/<str:end_date>/', book_now, name="book_now"),
-    path('doBooking/<int:id>/<str:start_date>/<str:end_date>/', do_booking, name="do_booking"),
+    path('doBooking/<int:id>/<str:start_date>/<str:end_date>/', save_booking, name="save_booking"),
     path('backToHome/', back_button, name="back_to_homepage"),
     path('myBookings/', my_bookings, name='my_bookings'),
-    path('cancelBooking/<int:id>/',cancel_booking, name='cancel_booking')
+    path('cancelBooking/<int:id>/', cancel_booking, name='cancel_booking')
 
 ]
--- offices/views.py
+++ offices/views.py
@@ -1,42 +1,55 @@
-from datetime import datetime
-
+from datetime import datetime, time
+from django.core.exceptions import ValidationError
 from django.shortcuts import render, get_object_or_404, reverse, redirect
-
 from .booking_functions.reservation_details import office_zone_details
 from .forms import AvailabilityForm
 from django.http import HttpResponse
 from django.db import IntegrityError
 from offices.models import Reservation
-from offices.booking_functions.availability import check_availability
+from offices.booking_functions.availability import check_availability, get_valid_dates
 from selection.models import Office, Zone
 from django.contrib.auth import get_user_model
 from django.core.mail import send_mail
+from django.contrib import messages
 
 AuthUserModel = get_user_model()
 
 
 def get_available_offices(request):
     if request.method == 'GET' and len(request.GET) > 0:
+
+        if request.GET['start_date'] < datetime.strftime(datetime.now(),'%Y-%m-%d'):
+            print(request.GET['start_date'],datetime.strftime(datetime.now(),'%Y-%m-%d'),'First IF')
+            messages.warning( request, 'The enter date must be grater than today' )
+            return redirect('/')
+        if request.GET['start_date'] > request.GET['end_date']:
+            messages.warning( request, 'Start Date must be grater than endate' )
+            return redirect('/')
+        if not get_valid_dates(request, request.GET['start_date'], request.GET['end_date']):
+            messages.warning(request, 'Do not overlap periods' )
+            return redirect('/')
+
         offices = list(Office.objects.values())
         available_offices = []
         for office in offices:
             if check_availability(office['id'], request.GET['start_date'], request.GET['end_date']):
                 available_offices.append(office)
+        return render( request, 'offices/availability.html',
+                       {'form_data': available_offices,
+                        'start_date': request.GET['start_date'],
+                        'end_date': request.GET['end_date']})
     else:
-        form = AvailabilityForm()
-    return render(request, 'offices/availability.html',
-                {'form_data': available_offices,
-                 'start_date': request.GET['start_date'],
-                 'end_date': request.GET['end_date']})
+        messages.warning(request, 'Something went wrong')
+        return redirect('/')
 
 
 def book_now(request, id, start_date, end_date):
-    office_details, zone_details = office_zone_details(id)
-    return render(request, 'offices/reservation.html', {'office_details': office_details, 'zone_details': zone_details,
+    office_to_book = Office.objects.get(id=id)
+    return render(request, 'offices/reservation.html', {'office_to_book': office_to_book,
                                                         'start_date': start_date, 'end_date': end_date})
 
 
-def do_booking(request, id, start_date, end_date):
+def save_booking(request, id, start_date, end_date):
     if request.method == 'POST':
         try:
             office_obj = Office.objects.get(id=id)
@@ -48,7 +61,7 @@ def do_booking(request, id, start_date, end_date):
             )
             send_mail(
                 'Office booking confirmation',
-                'You have succesfully reserverd ' f'{office_obj.name}',
+                'You have successfully reserved ' f'{office_obj.name}',
                 'razvan1989@gmail.com',
                 [request.user.email],
             )
@@ -63,30 +76,14 @@ def back_button(request):
 
 
 def my_bookings(request):
+    bookings = Reservation.objects.filter(start_date__gte=datetime.strftime(datetime.now(),'%Y-%m-%d'), user=request.user)
+    return render(request, 'offices/bookings.html',
+                  {'user_bookings': bookings})
 
-    bookings = Reservation.objects.filter(start_date__gte='2021-11-19')
-    my_bookings = bookings.filter(user=request.user)
-    booking_info = {}
-    booking_list = []
-    for booking in list(my_bookings.values()):
-        office_details, zone_details = office_zone_details(booking['office_id'])
-        booking_info['Office'] = office_details
-        booking_info['Zone'] = list(zone_details.values())[0]['name']
-        booking_info['Floor'] = list(zone_details.values())[0]['floor_id']
 
-        booking_info['Start Date'] = booking['start_date']
-        booking_info['End Date'] = booking['end_date']
-        booking_info['BookingId'] = booking['id']
-        booking_list.append(booking_info.copy())
-    return render(request, 'offices/bookings.html', {
-        'my_bookings': booking_list,
-
-    })
-
-def cancel_booking(requeset,id):
-    print(id)
+def cancel_booking(request, id):
     try:
         Reservation.objects.filter(id=id).delete()
-        return(HttpResponse(status=201))
+        return redirect('/')
     except Exception as ex:
         return(HttpResponse(status=400))
